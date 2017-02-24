293d7d99dd7a41e522755def6b838a048bddf2e8
Steven Antonio <pastrana.steven.az@gmail.com>
2017-02-24 13:20:40 -0700

    finished coffee

--- lesson_6/CoffeeShop/CoffeeShop.java
+++ lesson_6/CoffeeShop/CoffeeShop.java
@@ -6,6 +6,7 @@ public class CoffeeShop{
   static double TEA_PRICE = 2.75;
   static double SUGAR_PRICE = 0.50;
   static double CREAMER_PRICE = 0.50;
+  static double DOUBLE_SHOT_PRICE = 1.25;
 
   double runningTotal;
   boolean orderComplete = false;
@@ -25,7 +26,8 @@ public class CoffeeShop{
         caramelOptions();
         break;
         case 3: runningTotal = runningTotal + TEA_PRICE;
-        teaOptions();
+        System.out.println("Ordered a tea.");
+        isDone();
         break;
         default:
         System.out.println("Invalid option. Please try again.");
@@ -35,73 +37,109 @@ public class CoffeeShop{
     } while ( orderComplete == false);
     calcTotal();
   }
-  // The following are all the optional add on methods.
+  //  COFFEE OPTIONS ===================================//
   public void iceOptions(){
     System.out.println("Ordering coffee. Would you like ice for an additional $0.00?"+'\n'+"[y/n]");
     char iceChoice = i.next().charAt(0);
+    while (iceChoice != 'y' && iceChoice != 'n'){
+      System.out.println("Invalid option. Please try again.");
+      iceOptions();
+    }
     if (iceChoice == 'y'){
       System.out.println("Ice added to drink");
       creamerOptions();
-    }else if(iceChoice == 'n'){
+    }else {
       System.out.println("No ice selected");
       creamerOptions();
-    }else{
-      System.out.println("Invalid option. Please try again.");
-      iceOptions();
     }
   }
   public void creamerOptions(){
     System.out.println("Would you like Cream for an additional $0.50?"+'\n'+"[y/n]");
     char creamChoice = i.next().charAt(0);
-    if (creamChoice == 'y'){
-      runningTotal = runningTotal + CREAMER_PRICE;
-      System.out.println("Creamer added to drink.");
-      sugarOptions();
-    }else if (creamChoice == 'n'){
-      System.out.println("No creamer added");
-      sugarOptions();
-    }else {
+    while (creamChoice != 'y' && creamChoice != 'n'){
       System.out.println("Invalid option. Please try again.");
       creamerOptions();
+    }
+    if (creamChoice == 'y'){
+      runningTotal = runningTotal + CREAMER_PRICE;
+      System.out.println("Creamer added to drink");
+      sugarOptions();
+    }else {
+      System.out.println("No creamer selected");
+      sugarOptions();
     }
   }
   public void sugarOptions(){
     System.out.println("Would you like Sugar for an additional $0.50?"+'\n'+"[y/n]");
     char sugarChoice = i.next().charAt(0);
-    if (sugarChoice == 'y'){
-      runningTotal = runningTotal + SUGAR_PRICE;
-      System.out.println("Sugar added to drink.");
-      isDone();
-    }else if (sugarChoice == 'n'){
-      System.out.println("No sugar added");
-      isDone();
-    }else {
+    while (sugarChoice != 'y' && sugarChoice != 'n'){
       System.out.println("Invalid option. Please try again.");
       sugarOptions();
     }
+    if (sugarChoice == 'y'){
+      runningTotal = runningTotal + SUGAR_PRICE;
+      System.out.println("Sugar added to drink");
+      isDone();
+    }else {
+      System.out.println("No sugar selected");
+      isDone();
+    }
   }
+  // ESPRESSO OPTIONS ========================//
   public void caramelOptions(){
     System.out.println("Would you like caramel sweetener for an additional $0.00?"+'\n'+"[y/n]");
-    char sugarChoice = i.next().charAt(0);
-    if (sugarChoice == 'y'){
-      System.out.println("Caramel sweetener added to drink.");
-      chocolateOptions();
-    }else if (sugarChoice == 'n'){
-      System.out.println("No carmel sweetener added");
-      chocolateOptions();
-    }else {
+    char caramelChoice = i.next().charAt(0);
+    while (caramelChoice != 'y' && caramelChoice != 'n'){
       System.out.println("Invalid option. Please try again.");
       caramelOptions();
     }
+    if (caramelChoice == 'y'){
+      System.out.println("Creamer added to drink");
+      chocolateOptions();
+    }else {
+      System.out.println("No creamer selected");
+      chocolateOptions();
+    }
   }
   public void chocolateOptions(){
-
+    System.out.println("Would you like chocolate for an additional $0.00?"+'\n'+"[y/n]");
+    char chocolateChoice = i.next().charAt(0);
+    while (chocolateChoice != 'y' && chocolateChoice != 'n'){
+      System.out.println("Invalid option. Please try again.");
+      chocolateOptions();
+    }
+    if (chocolateChoice == 'y'){
+      System.out.println("Chocolate added to drink");
+      doubleShotOptions();
+    }else {
+      System.out.println("No chocolate selected");
+      doubleShotOptions();
+    }
   }
+  public void doubleShotOptions(){
+    System.out.println("Would you like a double shot for an additional $1.25?"+'\n'+"[y/n]");
+    char doubleShotChoice = i.next().charAt(0);
+    while (doubleShotChoice != 'y' && doubleShotChoice != 'n'){
+      System.out.println("Invalid option. Please try again.");
+      doubleShotOptions();
+    }
+    if (doubleShotChoice == 'y'){
+      runningTotal = runningTotal + DOUBLE_SHOT_PRICE;
+      System.out.println("Double shot selected");
+      isDone();
+    }else {
+      System.out.println("Single shot selected");
+      isDone();
+    }
+  }
+ // This method is called when the customer finalizes the order. It presents the total.
+ // I would like to present a list of everything ordered, but am unaware of how to doo that without using
+ // data structures, like arrays which we have not covered in class yet.
   public void calcTotal(){
-    System.out.println("Called calcTotal()");
+    System.out.println("Thank you for visiting. You total is $"+ String.format("%.2f", runningTotal));
   }
 
-  // Tis method is called when an order is completed. It asks the user if they are finished ordering.
+  // This method is called when an order is completed. It asks the user if they are finished ordering.
   public void isDone(){
     System.out.println('\n'+"Would you like something else?"+'\n'+"[y/n]");
     char orderStat = i.next().charAt(0);
